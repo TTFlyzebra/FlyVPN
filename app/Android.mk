9def5e3f7fb783af861dd2515dd980258705766e
flyzebra <flycnzebra@gmail.com>
2019-12-19 10:36:03 +0800

    add Android.mk

--- app/Android.mk
+++ app/Android.mk
@@ -2,17 +2,26 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES :=  gson-2.8.5:libs/gson-2.8.5.jar
+
 include $(BUILD_MULTI_PREBUILT)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_MANIFEST_FILE := src/main/AndroidManifest.xml
-LOCAL_SRC_FILES := $(call all-java-files-under, src/main/java)
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/src/main/res
-LOCAL_JAVA_LIBRARIES := framework
 LOCAL_STATIC_JAVA_LIBRARIES :=  gson-2.8.5 \
 								android-support-v4 \
-								android-support-v7-appcompat
-LOCAL_OVERRIDES_PACKAGES := mpApp
+								android-support-v7-appcompat \
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src/main/java)
+
+LOCAL_RESOURCE_DIR = $(LOCAL_PATH)/src/main/res \
+					 frameworks/support/v7/appcompat/res \
+
+LOCAL_AAPT_FLAGS :=  --auto-add-overlay \
+					 --extra-packages android.support.v7.appcompat \
+
+LOCAL_MANIFEST_FILE := src/main/AndroidManifest.xml
+
+#LOCAL_OVERRIDES_PACKAGES := mpApp
 #LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PACKAGE_NAME := NewMpApp
 LOCAL_CERTIFICATE := platform
