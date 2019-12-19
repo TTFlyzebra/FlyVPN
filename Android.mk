0f001b9142135654d107bb8d08dae718c6e1e94d
flyzebra <flycnzebra@gmail.com>
2019-12-19 14:33:36 +0800

    finish Android.mk

--- Android.mk
+++ Android.mk
@@ -1,31 +1,38 @@
-LOCAL_PATH := $(call my-dir)
+
+# Copyright 2007-2011 The Android Open Source Project
+
+LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_JAVA_LIBRARIES :=  gson-2.8.5 \
-								android-support-v4 \
-								android-support-v7-appcompat \
+LOCAL_STATIC_JAVA_LIBRARIES :=  android-support-v4
+LOCAL_STATIC_JAVA_LIBRARIES +=android-support-v7-appcompat
+LOCAL_STATIC_JAVA_LIBRARIES +=gson-2.8.5
 
-LOCAL_SRC_FILES := $(call all-java-files-under, $(LOCAL_PATH)/app/src/main/java) \
-                   $(call all-Iaidl-files-under, $(LOCAL_PATH)/app/src/main/aidl) \
+LOCAL_RESOURCE_DIR = \
+        $(LOCAL_PATH)/res \
+        frameworks/support/v7/appcompat/res
 
-LOCAL_RESOURCE_DIR = $(LOCAL_PATH)/app/src/main/res \
-					 frameworks/support/v7/appcompat/res \
+LOCAL_AAPT_FLAGS := \
+        --auto-add-overlay \
+        --extra-packages android.support.v7.appcompat \
 
-LOCAL_AAPT_FLAGS :=  --auto-add-overlay \
-					 --extra-packages android.support.v7.appcompat \
+LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main/java) \
+                   $(call all-Iaidl-files-under, app/src/main/aidl) \
 
 LOCAL_MANIFEST_FILE := app/src/main/AndroidManifest.xml
 
 LOCAL_OVERRIDES_PACKAGES := mpApp
-#LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PACKAGE_NAME := NewMpApp
 LOCAL_CERTIFICATE := platform
+#LOCAL_PRIVILEGED_MODULE := false
+#LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 LOCAL_PROGUARD_ENABLED := disabled
-
+LOCAL_DEX_PREOPT := false
 include $(BUILD_PACKAGE)
 
 include $(CLEAR_VARS)
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES :=  gson-2.8.5:app/libs/gson-2.8.5.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES :=gson-2.8.5:libs/gson-2.8.5.jar \
 
 include $(BUILD_MULTI_PREBUILT)
+include $(call all-makefiles-under, $(LOCAL_PATH))
