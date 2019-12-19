a6310eba029d7e63aca88ca6e36bd70933af064c
flyzebra <flycnzebra@gmail.com>
2019-12-19 11:57:59 +0800

    finish Android.mk

--- app/Android.mk
+++ app/Android.mk
@@ -1,18 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES :=  gson-2.8.5:libs/gson-2.8.5.jar
-
-include $(BUILD_MULTI_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_STATIC_JAVA_LIBRARIES :=  gson-2.8.5 \
 								android-support-v4 \
 								android-support-v7-appcompat \
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src/main/java) \
-                   $(call all-Iaidl-files-under, src/main/aidl) \
+LOCAL_SRC_FILES := $(call all-java-files-under, $(LOCAL_PATH)/src/main/java) \
+                   $(call all-Iaidl-files-under, $(LOCAL_PATH)/src/main/aidl) \
 
 LOCAL_RESOURCE_DIR = $(LOCAL_PATH)/src/main/res \
 					 frameworks/support/v7/appcompat/res \
@@ -29,3 +24,8 @@ LOCAL_CERTIFICATE := platform
 LOCAL_PROGUARD_ENABLED := disabled
 
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES :=  gson-2.8.5:libs/gson-2.8.5.jar
+
+include $(BUILD_MULTI_PREBUILT)
