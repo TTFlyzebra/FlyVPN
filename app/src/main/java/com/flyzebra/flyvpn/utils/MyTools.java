cafcb50b27839816e2624178c1352973fed685da
flyzebra <flycnzebra@gmail.com>
2019-12-21 13:08:27 +0800

    add testactivity

--- app/src/main/java/com/flyzebra/flyvpn/utils/MyTools.java
+++ app/src/main/java/com/flyzebra/flyvpn/utils/MyTools.java
@@ -2,7 +2,9 @@ package com.flyzebra.flyvpn.utils;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.UserHandle;
 
+import java.lang.reflect.Field;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
@@ -15,8 +17,22 @@ import java.util.Random;
  * Date: 19-12-10 上午9:56
  */
 public class MyTools {
+    private static UserHandle userHandle = null;
+    static {
+        try {
+            Class clazz = Class.forName("android.os.UserHandle");
+            Field field = clazz.getField("ALL");
+            userHandle = (UserHandle) field.get(clazz);//treeToolbar类
+        } catch (Exception e) {
+            userHandle = null;
+            e.printStackTrace();
+        }
+
+    }
+
     /**
      * 根据当前时间生成SessionID
+     *
      * @return
      */
     public static synchronized int createSessionId() {
@@ -36,7 +52,11 @@ public class MyTools {
         intent.putExtra("NETWORK_LINK_WIFI", wifi ? 1 : 0);
         intent.putExtra("NETWORK_LINK_4G", mobile ? 1 : 0);
         intent.putExtra("NETWORK_LINK_MCWILL", mcwill ? 1 : 0);
-        context.sendBroadcast(intent);
+        if(userHandle!=null){
+            context.sendBroadcastAsUser(intent,userHandle);
+        }else{
+            context.sendBroadcast(intent);
+        }
     }
 
 }
